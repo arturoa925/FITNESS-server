845f36c812d30bc2157dcba1affaac3aa5d0f6a3
kalisurf3r <ayalaarturo925@gmail.com>
2025-08-07 15:31:10 -0700

    resolved config, db, server files

---
+++ config/connection.js
@@ -0,0 +1,25 @@
+const Sequelize = require("sequelize");
+require("dotenv").config();
+
+const sequelize = process.env.DATABASE_URL
+  ? new Sequelize(process.env.DATABASE_URL, {
+      dialect: "postgres",
+      protocol: "postgres",
+      dialectOptions: {
+        ssl: {
+          require: true, // Ensure SSL is required
+          rejectUnauthorized: false, // For self-signed certificates (like Render's PostgreSQL)
+        },
+      },
+      pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000, // Increase timeout here (default is 10000ms)
+        idle: 10000,
+      },
+    })
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+      host: "localhost",
+      dialect: "postgres",
+    });
+module.exports = sequelize;
