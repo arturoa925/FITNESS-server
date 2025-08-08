e13f78525ee065a78e9194a9a554f757d41d5d9f
kalisurf3r <ayalaarturo925@gmail.com>
2025-08-07 18:20:48 -0700

    updated models index

--- models/index.js
+++ models/index.js
@@ -1,5 +1,43 @@
 const Users = require("./Users");
 const Calendar = require("./Calendar");
 const DailyWorkout = require("./DailyWorkout");
-const TrainingPrograms = require("./TrainingPrograms");
+const TrainingPrograms = require("./trainingPrograms");
 
+// Associations
+
+Users.hasOne(TrainingPrograms, {
+    foreignKey: "userId",
+    onDelete: "CASCADE",
+});     
+
+TrainingPrograms.belongsTo(Users, {
+    foreignKey: "userId",
+    onDelete: "CASCADE",
+});
+
+Users.hasOne(Calendar, {
+    foreignKey: "userId",
+    onDelete: "CASCADE",
+});
+
+Calendar.belongsTo(Users, {
+    foreignKey: "userId",
+    onDelete: "CASCADE",
+});
+
+Users.hasOne(DailyWorkout, {
+    foreignKey: "userId",
+    onDelete: "CASCADE",
+});
+
+DailyWorkout.belongsTo(Users, {
+    foreignKey: "userId",
+    onDelete: "CASCADE",
+});
+
+module.exports = {
+    Users,
+    Calendar,
+    DailyWorkout,
+    TrainingPrograms,
+};
